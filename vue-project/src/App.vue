2737219c10a3001aa2fd4f40ada447a0b02a3728
FerchoApp <lfgalindoq@ufpso.edu.co>
2023-11-01 16:22:52 -0500

    notificationsExample

---
+++ vue-project/src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { ref } from 'vue';
+
+let qty = ref(0)
+
+function addValue(){
+  qty.value++
+}
+function minusValue(){
+  if(qty.value>0){
+    qty.value--
+  }
+}
+</script>
+
+<template>
+ <div class="wrapper">
+  <div class="profile">
+    <div class="ballon">{{ qty }}</div>
+  </div>
+  <div class="buttom-wrapper">
+    <div class="buttom" @click="addValue">+</div>
+    <div class="buttom" @click="minusValue">-</div>
+  </div>
+ </div>
+</template>
+
+<style scoped>
+.buttom{
+  width: 32px;
+  background-color: #F2CB05;
+  display: inline-block;
+  padding: 4px 0;
+  border-radius: 4px;
+  margin: 0 8px;
+}
+.buttom:hover{
+cursor: pointer;
+background-color: #F23030;
+}
+.buttom-wrapper{
+margin-top: 16px;
+margin-bottom: 16px;
+text-align: center;
+}
+.ballon{
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  width: 32px;
+  height: 32px;
+  background-color: #F28705;
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+.profile{
+  background-image: url(https://pbs.twimg.com/profile_images/520777752983519232/SD31s4zo_400x400.jpeg) ;
+  background-size: cover;
+  position: relative;
+  background-color: orange;
+  width: 120px;
+  height: 120PX;
+  margin: 0 auto;
+  border-radius: 100%;
+  margin-top: 16px;
+
+}
+.wrapper{
+  margin-top: 18px;
+  background-color: #93D94E;
+  width: 60%;
+  border-radius: 12px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+</style>
